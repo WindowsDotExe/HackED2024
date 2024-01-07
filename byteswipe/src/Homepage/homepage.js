e9293d74992bf0f97c54a71112010bf5a2455a83
sarveshXrajan <sarveshr03@gmail.com>
2024-01-06 17:15:29 -0700

    homepage template done

---
+++ byteswipe/src/Homepage/homepage.js
@@ -0,0 +1,20 @@
+import './homepage.css'
+
+function Homepage() {
+    return (
+        <body>
+         <link href='https://fonts.googleapis.com/css?family=Lexend Deca' rel='stylesheet'></link>
+
+         <h1> ByteSwipe! </h1> 
+          
+         <h2> This is dummy text. This is more dummy text. This is more dummy text. </h2>
+        
+        <div className='button-container'>
+         <button > Register </button>
+         <button > Login </button></div>
+        
+        </body>
+    );
+    }
+
+export default Homepage;
